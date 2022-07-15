0018d98ca07717c5c2e0dc52d16b7ffd1cc4348f
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-07-14 22:37:36 -0300

    creating http's routes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,32 @@ const Koa = require("koa");
 const Router = require("koa-router");
 
 const router = new Router();
-const server = new Koa();
+const app = new Koa();
 
 router
   .get("/", (ctx) => {
-    ctx.body = "<h1>Welcome!</h1>";
+    ctx.body = { message: "Hello!" };
   })
-  .get("/contato", (ctx) => {
-    ctx.body = "<h1>Contato</h1>";
+  .get("/courses", (ctx) => {
+    const query = ctx.query;
+    console.log(query);
+    ctx.body = ["course 1", "course 2", "course 3"];
+  })
+  .post("/courses", (ctx) => {
+    ctx.body = ["course 1", "course 2", "course 3", "course 4"];
+  })
+  .put("/courses/:id", (ctx) => {
+    const { id } = ctx.params;
+    console.log(id);
+    ctx.body = ["course 6", "course 2", "course 3", "course 4"];
+  })
+  .patch("/courses/:id", (ctx) => {
+    ctx.body = ["course 6", "course 7", "course 3", "course 4"];
+  })
+  .delete("/courses/:id", (ctx) => {
+    ctx.body = ["course 6", "course 7", "course 3", "course 4"];
   });
 
-server.use(router.routes());
+app.use(router.routes());
 
-server.listen(3000, () => console.log("listening on port 3000"));
+app.listen(3000);
